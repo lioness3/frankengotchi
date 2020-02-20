eb7e2c5fa187db891663dfcdea87651f6f834b26
Student <example@example.com>
2020-02-20 09:43:39 -0800

    add og state of green

--- src/components/Score.js
+++ src/components/Score.js
@@ -3,87 +3,33 @@ import PropTypes from "prop-types";
 
 import './Score.css';
 function Score(props){
+  let [health, setHealth] = useState(10);
+  let [sleep, setSleep] = useState(10);
+  let btnColor = "green"
+  let [color, setColor] = useState('green');
 
-// let countDown =   setInterval(() => {
-//       setTimer( timer -=1)
-//     }, 1000);
+  let healthCountDown = () => {
 
-    useEffect(() => {
-  const interval = setInterval(() => {
+    if (health < 1 ) { return }
+    setTimeout(()=>{
+      setHealth(health - 1, healthCountDown)
+    },1000)
+  }
 
-    setSeconds(seconds  => seconds - 1);
-  }, 1000);
-
-  return () => clearInterval(interval);
-}, []);
-
-  // let [timer, setTimer] = useState(5);
-  // if(timer == 0){
-  //   console.log(timer);
-  // countDown = clearInterval();
-
-
-
-  let [seconds, setSeconds] = useState(10000000);
-  useEffect(() => {
-const interval = setInterval(() => {
-
-  setSeconds(seconds  => seconds - 1);
-}, 1000);
-
-return () => clearInterval(interval);
-}, []);
-
-
-  let [health, setHealth] = useState(10000000);
-  useEffect(() => {
-const interval = setInterval(() => {
-
-  setHealth(health  => health - 1);
-}, 1000);
-
-return () => clearInterval(interval);
-}, []);
-
-
-  let [love, setLove] = useState(10000000);
-  useEffect(() => {
-const interval = setInterval(() => {
-
-  setLove(love  => love - 1);
-}, 1000);
-
-return () => clearInterval(interval);
-}, []);
-
-
-let [sleep, setSleep] = useState(10000000);
-useEffect(() => {
-const interval = setInterval(() => {
-
-setSleep(sleep  => sleep - 1);
-}, 1000);
-
-return () => clearInterval(interval);
-}, []);
-
-const [counter, setCounter] = useState(0);
-
+healthCountDown()
 
   return(
     <div className='healthBox'>
     <h1> Score Nonsense</h1>
-    <h1>Level:{counter}</h1>
-        <button onClick={() => setCounter(counter + 1)}  > Level UP </button>
 
-    <h1> Health Meter:{health} <button onClick={() => setHealth(health + 5)}  > Level UP </button></h1>
 
-    <h1>Love Meter:{love} <button onClick={() => setLove(love + 5)}  > Level UP </button></h1>
+    <h1> <span style={{backgroundColor:color}}>Health Meter:{health}</span> </h1>
+    <h1> Sleep Meter:{sleep} <button onClick={() => setSleep(sleep + 5)}  > Level UP </button></h1>
 
-    <h1>Sleep Meter:{sleep} <button onClick={() => setSleep(sleep + 5)}  > Level UP </button></h1>
     </div>
   )
 }
+
 
 Score.propTypes={
 level:PropTypes.number,
